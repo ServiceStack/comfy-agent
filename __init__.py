f3ce5cb7cdffb17780dff8cfe1257997c40bb7b6
Demis Bellot <demis.bellot@gmail.com>
2025-09-24 10:26:25 +0800

    sync

--- __init__.py
+++ __init__.py
@@ -1,21 +1,20 @@
-import os
 from .comfy_agent_node import RegisterComfyAgentNode
 from .require_nodes import RegisterRequireNodes
+from .llms_agent import RegisterLLMsAgentNode
 
 NODE_CLASS_MAPPINGS = {
     **RegisterComfyAgentNode.NODE_CLASS_MAPPINGS,
     **RegisterRequireNodes.NODE_CLASS_MAPPINGS,
+    **RegisterLLMsAgentNode.NODE_CLASS_MAPPINGS,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     **RegisterComfyAgentNode.NODE_DISPLAY_NAME_MAPPINGS,
     **RegisterRequireNodes.NODE_DISPLAY_NAME_MAPPINGS,
+    **RegisterLLMsAgentNode.NODE_DISPLAY_NAME_MAPPINGS,
 }
-
-WEB_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), "js")
 
 __all__ = [
     "NODE_CLASS_MAPPINGS",
     "NODE_DISPLAY_NAME_MAPPINGS",
-    "WEB_DIRECTORY"
 ]
